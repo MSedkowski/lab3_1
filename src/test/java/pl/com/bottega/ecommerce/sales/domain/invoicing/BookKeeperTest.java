1b0edeb9a82e68aea24920466f0e6ac50ac79529
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2018-04-15 19:29:01 +0200

    Create test for second test case.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -8,6 +8,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -18,8 +19,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookKeeperTest {
@@ -44,7 +44,6 @@ public class BookKeeperTest {
     @Test
     public void createInvoiceWithOnePosition() {
         productData = mock(ProductData.class);
-        Tax tax = mock(Tax.class);
         Money net = new Money(10);
         RequestItem item = new RequestItem(productData, 10, net);
         List<RequestItem> listOfItems = new ArrayList<>();
@@ -52,10 +51,32 @@ public class BookKeeperTest {
         when(productData.getType()).thenReturn(ProductType.FOOD);
         when(invoiceRequest.getClientData()).thenReturn(client);
         when(invoiceRequest.getItems()).thenReturn(listOfItems);
-        when(taxPolicy.calculateTax(ProductType.FOOD, net)).thenReturn(tax);
-        when(tax.getAmount()).thenReturn(net);
+        when(taxPolicy.calculateTax(ProductType.FOOD, net)).thenReturn(new Tax(net, "jedzenie"));
         Invoice newInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(newInvoice.getItems().size(), is(1));
     }
+
+    @Test
+    public void createInvoiceWithTwoPosition() {
+        productData = mock(ProductData.class);
+        ProductData productData2 = mock(ProductData.class);
+        Money net = new Money(10);
+        RequestItem item = new RequestItem(productData, 10, net);
+        RequestItem item2 = new RequestItem(productData2, 12, net);
+        List<RequestItem> listOfItems = new ArrayList<>();
+        listOfItems.add(item);
+        listOfItems.add(item2);
+        when(productData.getType()).thenReturn(ProductType.FOOD);
+        when(productData2.getType()).thenReturn(ProductType.DRUG);
+        when(invoiceRequest.getClientData()).thenReturn(client);
+        when(invoiceRequest.getItems()).thenReturn(listOfItems);
+        when(taxPolicy.calculateTax(productData.getType(), net)).thenReturn(new Tax(net, "jedzenie"));
+        when(taxPolicy.calculateTax(productData2.getType(), net)).thenReturn(new Tax(net, "leki"));
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(taxPolicy, times(1)).calculateTax(productData.getType(), net);
+        verify(taxPolicy, times(1)).calculateTax(productData2.getType(), net);
+    }
+
+    
 
 }
